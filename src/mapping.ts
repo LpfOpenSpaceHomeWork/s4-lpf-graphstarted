fedd118a78eb90915f0ca48f463a4bd8a4847747
runnan <1608272694@qq.com>
2024-10-31 15:29:11 +0800

    feat: 实现在mapping中查询合约

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,13 +1,16 @@
 import { NFTCreated as NFTCreatedEvent } from "../generated/NFTFactory/NFTFactory";
 import { CreatedNFT } from "../generated/schema";
-import { NFT } from "../generated/templates";
+import { NFT as NFTDatasource } from "../generated/templates";
 import { ByteArray, crypto } from "@graphprotocol/graph-ts";
 import { TokenInfo } from "../generated/schema";
-import { Transfer as TokenTransferEvent } from "./../generated/templates/NFT/NFT";
+import {
+  NFT,
+  Transfer as TokenTransferEvent,
+} from "./../generated/templates/NFT/NFT";
 
 export function handleNFTCreated(event: NFTCreatedEvent): void {
   // 实例化NFT合约数据源
-  NFT.create(event.params.nftCA);
+  NFTDatasource.create(event.params.nftCA);
   // 在store中创建NFT实体
   const nftEntity = new CreatedNFT(event.params.nftCA.toHexString()); // 直接用合约地址做ID
   nftEntity.nftCA = event.params.nftCA;
@@ -29,13 +32,16 @@ export function handleTokenTransfer(event: TokenTransferEvent): void {
   );
   let tokenEntity = TokenInfo.load(tokenEntityID.toHexString());
   if (!tokenEntity) {
+    const nftContract = NFT.bind(nftCA);
     tokenEntity = new TokenInfo(tokenEntityID.toHexString());
+    tokenEntity.ca = nftCA;
+    tokenEntity.tokenId = tokenID;
+    tokenEntity.name = nftContract.name();
+    tokenEntity.tokenURL = nftContract.tokenURI(tokenID);
     tokenEntity.blockNumber = event.block.number;
     tokenEntity.blockTimestamp = event.block.timestamp;
     tokenEntity.transactionHash = event.transaction.hash;
   }
-  tokenEntity.ca = nftCA;
-  tokenEntity.tokenId = tokenID;
   tokenEntity.owner = event.params.to;
   tokenEntity.save();
 }
